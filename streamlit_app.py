1eddfd16b115ebab3423f648143be5decac55c25
Jakub Kaminiarz <69792705+jakub-kaminiarz@users.noreply.github.com>
2022-10-27 09:17:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 # Display normalized table on the page.
 streamlit.dataframe(fruityvice_normalized)
+
+import snowfalke.connector
